b693dcd6c82b63366d4a29fad838c73e8e9f46f1
V Niranjan <vniranjan72@outlook.com>
2023-06-24 08:49:01 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,46 +1,13 @@
-import java.io.IOException;
-import java.sql.*;
-import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-@WebServlet("/LoginServlet")
-public class Employee extends HttpServlet {
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception e) {}
-    }
+public class Employee {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        boolean success = false;
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        // Unsafe query which uses string concatenation
-        String query = "select * from tbluser where username='" + username + "' and password = '" + password + "'";
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/user", "root", "root");
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            if (rs.next()) {
-                // Login Successful if match is found
-                success = true;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                stmt.close();
-                conn.close();
-            } catch (Exception e) {}
-        }
-        if (success) {
-            response.sendRedirect("home.html");
-        } else {
-            response.sendRedirect("login.html?error=1");
-        }
-    }
+  public static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    System.out.println("Hello, World! The current time is " + dtf.format(now));
+  }
+
 }
